04baf54a6ee68bb8d46e2bed40750bb3f367217b
LG <LG@DESKTOP-HeeJung>
2020-09-04 01:22:55 +0900

    222

--- src/kosta/mvc/view/TestView.java
+++ src/kosta/mvc/view/TestView.java
@@ -4,5 +4,8 @@ public class TestView {
    public void test() {
 	   System.out.println("javaWork에서 수정한다..");
 	   System.out.println("boot에서 수정한다..");
+	   
+	   System.out.println("또 해보자...");
+		  System.out.println("해볻..");
    }
 }
