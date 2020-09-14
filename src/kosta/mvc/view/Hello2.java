72a28a4cc3b1c9ced59a10862fd8bba45ad6cafd
kosta <kosta@192.168.0.136>
2020-09-14 12:11:05 +0900

    왼쪽 pc 수정

---
+++ src/kosta/mvc/view/Hello2.java
@@ -0,0 +1,10 @@
+package kosta.mvc.view;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/kosta/mvc/view/TestView.java
+++ src/kosta/mvc/view/TestView.java
@@ -11,6 +11,9 @@ public class TestView {
 
 	  System.out.println("또 해보자...");
 	  System.out.println("해볻..");
+	  
+	  System.out.println("왼쪽 pc에서 추가했어요.");
+	  System.out.println("111");
 
    }
 }
